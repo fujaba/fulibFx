a69155714e8ef5000143d6741d05aa58474a81c1
UeberallGebannt <paulmertens63@gmail.com>
2023-11-22 09:39:01 +0100

    feat: Update examples and include for loops

--- example/src/main/java/io/github/sekassel/person/controller/PersonController.java
+++ example/src/main/java/io/github/sekassel/person/controller/PersonController.java
@@ -1,8 +1,12 @@
 package io.github.sekassel.person.controller;
 
-import io.github.sekassel.person.backend.Person;
 import io.github.sekassel.jfxframework.controller.annotation.Controller;
-import javafx.beans.property.ObjectProperty;
+import io.github.sekassel.jfxframework.controller.ControllerEvent;
+import io.github.sekassel.person.backend.Person;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 import javax.inject.Inject;
@@ -10,24 +14,38 @@ import javax.inject.Inject;
 @Controller(view = "view/sub/person.fxml")
 public class PersonController extends VBox {
 
-    private ObjectProperty<Person> person;
+    @FXML
+    public Label lastName;
+    @FXML
+    public Label age;
+    @FXML
+    public ImageView image;
+    @FXML
+    public Label firstName;
+
+    private Person person;
 
     @Inject
     public PersonController() {
         super();
     }
 
-    public void setPerson(ObjectProperty<Person> person) {
-        this.person = person;
-    }
-
-    public ObjectProperty<Person> getPerson() {
+    public Person getPerson() {
         return person;
     }
 
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 
+    @ControllerEvent.onRender
+    public void onRender() {
+        firstName.setText(person.firstName());
+        lastName.setText(person.lastName());
+        age.setText(String.valueOf(person.age()));
+        image.setImage(new Image(person.image()));
 
-
+    }
 
 
 }
--- example/src/main/java/io/github/sekassel/person/controller/PersonListController.java
+++ example/src/main/java/io/github/sekassel/person/controller/PersonListController.java
@@ -1,15 +1,16 @@
 package io.github.sekassel.person.controller;
 
+import io.github.sekassel.jfxframework.constructs.For;
 import io.github.sekassel.jfxframework.controller.ControllerEvent;
 import io.github.sekassel.jfxframework.controller.annotation.Controller;
 import io.github.sekassel.person.backend.Person;
 import javafx.application.Platform;
-import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import javax.inject.Inject;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -21,11 +22,14 @@ public class PersonListController {
     public Label label;
 
     @FXML
-    public HBox controller;
+    public VBox nodeList;
+
+    @FXML
+    public VBox controllerList;
 
     ObservableList<Person> personList = FXCollections.observableArrayList(
-            new Person("", "Jocky", "Lowell", 42),
-            new Person("", "Jason", "Howdey", 34)
+            new Person("https://cdn-icons-png.flaticon.com/512/2815/2815428.png", "Jocky", "Lowell", 42),
+            new Person("https://cdn-icons-png.flaticon.com/512/2815/2815428.png", "Jason", "Howdey", 34)
     );
 
     public ObservableList<Person> getPersonList()
@@ -39,16 +43,30 @@ public class PersonListController {
 
     @ControllerEvent.onRender
     public void onRender() {
-        // Simulate adding new persons
+
+        Button button = new Button("Text");
+        button.setOnMouseClicked(event -> System.out.println("Hello"));
+
+        For.create(nodeList, personList, new VBox(button, new Label("Label")));
+        For.create(controllerList, personList, PersonController.class);
+
         new Thread(() -> {
             AtomicInteger i = new AtomicInteger();
             while (true) {
                 try {
-                    Thread.sleep(5000);
+                    Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                Platform.runLater(() -> personList.add(new Person("", "Person" + i.incrementAndGet(), "Mayer", i.get())));
+                Platform.runLater(() -> {
+                    System.out.println(i.get());
+                    if (i.get() % 2 == 0) {
+                        personList.remove(0);
+                    }
+                    else
+                        personList.add(new Person("https://cdn-icons-png.flaticon.com/512/2815/2815428.png", "Person" + i.get(), "Mayer", i.get()));
+                });
+                i.incrementAndGet();
             }
         }).start();
     }
