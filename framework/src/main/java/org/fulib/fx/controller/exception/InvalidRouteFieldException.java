8523fae634aa39600f99eedf4603ecd6e164bd57
LeStegii <paulmertens63@gmail.com>
2024-02-05 15:49:59 +0100

    fix: Remove mention of removed feature

--- framework/src/main/java/org/fulib/fx/controller/exception/InvalidRouteFieldException.java
+++ framework/src/main/java/org/fulib/fx/controller/exception/InvalidRouteFieldException.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Field;
 public class InvalidRouteFieldException extends RuntimeException {
 
     public InvalidRouteFieldException(Field field) {
-        super("Field %s in %s is annotated with @Route or @Providing but is not a Provider<T> providing a controller.".formatted(field.getName(), field.getDeclaringClass().getName()));
+        super("Field %s in %s is annotated with @Route but is not a Provider<T> providing a controller.".formatted(field.getName(), field.getDeclaringClass().getName()));
     }
 
 }
