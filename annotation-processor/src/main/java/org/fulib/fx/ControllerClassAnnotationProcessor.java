79aec4b3db6761faddc7e8df8da2d4a7069a341d
Adrian Kunz <a.kunz@uni-kassel.de>
2024-02-09 13:17:12 +0100

    feat: Check @Component view value for existing resource

--- annotation-processor/src/main/java/org/fulib/fx/ControllerClassAnnotationProcessor.java
+++ annotation-processor/src/main/java/org/fulib/fx/ControllerClassAnnotationProcessor.java
@@ -4,35 +4,67 @@ import com.google.auto.service.AutoService;
 import org.fulib.fx.annotation.controller.Component;
 import org.fulib.fx.annotation.controller.Controller;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+import java.io.IOException;
 import java.util.Set;
 
 @SupportedAnnotationTypes({
-        "org.fulib.fx.annotation.controller.*"
+	"org.fulib.fx.annotation.controller.*"
 })
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
-@AutoService(javax.annotation.processing.Processor.class)
-public class ControllerClassAnnotationProcessor extends AbstractProcessor {
+@AutoService(Processor.class)
+public class ControllerClassAnnotationProcessor extends AbstractProcessor
+{
 
-    @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv)
+	{
 
-        System.out.println("test");
+		for (Element element : roundEnv.getElementsAnnotatedWith(Component.class))
+		{
+			checkComponent(element);
+		}
 
-        for (Element element : roundEnv.getElementsAnnotatedWith(Component.class)) {
-            if (element.getAnnotation(Controller.class) != null) {
-                processingEnv.getMessager().printMessage(javax.tools.Diagnostic.Kind.ERROR, "A class cannot be annotated with @Controller and @Component.", element);
-            }
+		return true;
+	}
 
-            System.out.println(element + " " + element.asType());
-        }
+	private void checkComponent(Element element)
+	{
+		final String view = element.getAnnotation(Component.class).view();
+		if (!view.isEmpty())
+		{
+			checkView(element, view);
+		}
 
-        return true;
-    }
+		if (element.getAnnotation(Controller.class) != null)
+		{
+			processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+				"A class cannot be annotated with @Controller and @Component.",
+				element);
+		}
+	}
+
+	private void checkView(Element element, String view)
+	{
+		try
+		{
+			final FileObject resource = processingEnv.getFiler().getResource(StandardLocation.SOURCE_PATH,
+				processingEnv.getElementUtils().getPackageOf(element).getQualifiedName().toString(), view);
+			System.out.println(resource);
+		}
+		catch (IOException e)
+		{
+			processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+				"View file not found: " + view,
+				element);
+			processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
+				"(or source path not set, see https://stackoverflow.com/a/74159042)");
+		}
+	}
 }
