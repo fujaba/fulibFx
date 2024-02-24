127a95b13adb2d18a9bcfda6d0f5838a9f2cbf3a
LeStegii <paulmertens63@gmail.com>
2024-02-24 17:16:44 +0100

    fix(framework): Change incorrect message

--- framework/src/main/java/org/fulib/fx/controller/Router.java
+++ framework/src/main/java/org/fulib/fx/controller/Router.java
@@ -96,7 +96,7 @@ public class Router {
         // Check if the route exists and has a valid controller
         if (!this.routes.containsPath(route)) {
             if (FrameworkUtil.runningInDev() && this.routes.containsPath("/" + route))
-                FulibFxApp.LOGGER.warning("This route doesn't exist. Did you mean '/%s' instead of '%s'?".formatted(route, route));
+                FulibFxApp.LOGGER.warning("This route doesn't exist. Did you mean '/%s'?".formatted(route));
             throw new ControllerInvalidRouteException(route);
         }
 
