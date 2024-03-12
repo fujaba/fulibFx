f2324b677b6a28c897c9a6db35a7a37ccf0497df
LeStegii <paulmertens63@gmail.com>
2024-03-12 16:12:46 +0100

    test(framework): Try to fix headless ludo tests

--- ludo/src/test/java/de/uniks/ludo/AppTest.java
+++ ludo/src/test/java/de/uniks/ludo/AppTest.java
@@ -1,8 +1,9 @@
 package de.uniks.ludo;
 
+import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseButton;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.mockito.Spy;
@@ -26,10 +27,11 @@ public class AppTest extends ApplicationTest {
 
     @Test
     public void test() {
-        moveTo("2");;
-        moveBy(0, -20);
-        press(MouseButton.PRIMARY);
-        release(MouseButton.PRIMARY);
+        Platform.runLater(() -> app.stage().requestFocus());
+        waitForFxEvents();
+
+        press(KeyCode.LEFT);
+        release(KeyCode.LEFT);
         clickOn("#startButton");
 
         waitForFxEvents();
@@ -247,7 +249,7 @@ public class AppTest extends ApplicationTest {
     }
 
     private void roll() {
-        clickOn("#eyesLabel");;
+        clickOn("#eyesLabel");
         sleep(1100);
     }
 
