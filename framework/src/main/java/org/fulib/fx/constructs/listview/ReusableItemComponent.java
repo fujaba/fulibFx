b7213a1a0928693f09ec789b299c53a03c50e318
LeStegii <paulmertens63@gmail.com>
2024-03-24 21:56:10 +0100

    docs(framework): Add note about responsibility

--- framework/src/main/java/org/fulib/fx/constructs/listview/ReusableItemComponent.java
+++ framework/src/main/java/org/fulib/fx/constructs/listview/ReusableItemComponent.java
@@ -5,6 +5,8 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Interface for components that can be reused in a list view.
  * If a component implements this interface, it can be reused when its item changes instead of being destroyed and recreated.
+ * <p>
+ * The component is fully responsible for updating its display when the item changes.
  *
  * @param <T> The type of the item that the component can display
  */
