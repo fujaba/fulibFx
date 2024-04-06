36e035be6a31f24319b5545d5a7e5a8c79e88611
Adrian Kunz <clashsoft@hotmail.com>
2024-04-06 20:48:39 +0200

    fix(processor): Remove printStackTrace

--- annotation-processor/src/main/java/org/fulib/fx/FxClassGenerator.java
+++ annotation-processor/src/main/java/org/fulib/fx/FxClassGenerator.java
@@ -65,7 +65,6 @@ public class FxClassGenerator {
                 this.generateSidecar(out, componentClass);
             }
         } catch (IOException e) {
-            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
