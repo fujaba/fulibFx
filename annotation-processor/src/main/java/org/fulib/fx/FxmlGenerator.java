16f9d58c8be0223a66e5ff17167c59a8fb3483e4
Adrian Kunz <a.kunz@uni-kassel.de>
2024-04-12 11:21:02 +0200

    refactor(processor): Use SAX for FXML generator

--- annotation-processor/src/main/java/org/fulib/fx/FxmlGenerator.java
+++ annotation-processor/src/main/java/org/fulib/fx/FxmlGenerator.java
@@ -1,15 +1,21 @@
 package org.fulib.fx;
 
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.tools.FileObject;
-import javax.xml.namespace.QName;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.ProcessingInstruction;
-import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.Reader;
 
@@ -39,35 +45,51 @@ public class FxmlGenerator {
     }
 
     public void generateRenderFxml(PrintWriter out, FileObject resource) {
-        try (final Reader reader = resource.openReader(true)) {
-            final XMLEventReader xmlReader = XMLInputFactory.newInstance().createXMLEventReader(reader);
+        try (final InputStream inputStream = resource.openInputStream()) {
+            final SAXParserFactory factory = SAXParserFactory.newInstance();
 
-            boolean hasRoot = false;
-            while (xmlReader.hasNext()) {
-                final XMLEvent event = xmlReader.nextEvent();
-                if (event.isStartElement()) {
-                    StartElement startElement = event.asStartElement();
-                    System.out.println(startElement);
-                    if (!hasRoot) {
-                        hasRoot = true;
-                        if ("fx".equals(startElement.getName().getPrefix()) && "root".equals(startElement.getName().getLocalPart())) {
-                            final String rootType = startElement.getAttributeByName(QName.valueOf("type")).getValue();
-                            out.printf("    final %1$s _0 = (%1$s) root;%n", rootType);
-                        } else {
-                            final String rootType = startElement.getName().getLocalPart();
-                            out.printf("    final %1$s _0 = new %1$s();%n", rootType);
-                        }
-                    }
-                }
+            // https://rules.sonarsource.com/java/RSPEC-2755
+            // prevent XXE, completely disable DOCTYPE declaration:
+            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+
+            final SAXParser saxParser = factory.newSAXParser();
+
+            final DefaultHandler handler = new FxmlVisitor(out);
+
+            saxParser.parse(inputStream, handler);
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    class FxmlVisitor extends DefaultHandler {
+        private final PrintWriter out;
+        private int varCounter;
+
+        FxmlVisitor(PrintWriter out) {
+            this.out = out;
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) {
+            System.out.println("startElement: " + qName + " " + attributes);
+
+            if ("fx:root".equals(qName)) {
+                final String rootType = attributes.getValue("type");
+                out.printf("    final %1$s _%2$d = (%1$s) root;%n", rootType, varCounter++);
+            } else {
+                final String rootType = qName;
+                out.printf("    final %1$s _%2$d = new %1$s();%n", rootType, varCounter++);
             }
+        }
 
-            if (hasRoot) {
+        @Override
+        public void endDocument() {
+            if (varCounter > 0) {
                 out.println("    return _0;");
             } else {
                 out.println("    return null;");
             }
-        } catch (XMLStreamException | IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
